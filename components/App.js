22d865969138d074e567548d822f33fe9f652f91
Ketan Sahu <rorozoro9888@gmail.com>
2023-06-06 22:54:47 +0530

    first commit

---
+++ components/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./components/navbar"
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Navbar/>);
---
+++ components/navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function Navbar() {
+    <div>
+        <h1>hii react</h1>
+    </div>
+}
